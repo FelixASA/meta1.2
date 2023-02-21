1c73f838e4b013c3b65607ba301fb3895fe9afae
FelixASA <felix.arredondo@uabc.edu.mx>
2023-02-21 10:52:19 -0700

    Initial commit

---
+++ src/components/pagina2.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">
+          Autor
+        </th>
+        <th class="text-left">
+          Titulo
+        </th>
+        <th class="text-left">
+          Post
+        </th>
+      </tr>
+    </thead>
+    <tbody class="text-h6 text-md-h5 text-lg-h5">
+      <tr
+        v-for="item in titles"
+        :key="item.titles"
+      >
+        <td>{{ autores[item.userId-1].name }}</td>
+        <td>{{ item.title }}</td>
+        <td>{{ item.body }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+const titles = []
+const autores = []
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => {
+  for (let i =0; i< json.length ;i++) {
+   titles.push(json[i])
+  }
+  });
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) => {
+  for (let i =0; i< json.length ;i++) {
+   autores.push(json[i])
+  }
+  });
+
+
+
+  export default {
+    data () {
+      return {
+        titles, autores,
+      }
+    },
+  }
+</script>
